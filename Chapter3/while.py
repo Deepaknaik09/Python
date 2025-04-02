c95abf7839b1ea5851f173bc988fcd0a5b255ded
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-02 07:09:41 +0530

    while condition

---
+++ Chapter3/while.py
@@ -0,0 +1,13 @@
+# count = 1
+# while count <= 5:
+#     print("hello world")
+#     count = count + 1  #count += 1
+    
+# print(count)
+    
+i = 1
+while i <= 5 :
+    print("Python")
+    i += 1
+    
+print("while loop endded")
