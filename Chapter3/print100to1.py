dfb22b08b5a82a1b883584db385da281856c2ca7
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-02 07:34:56 +0530

    printing list using while

--- Chapter3/print100to1.py
+++ Chapter3/print100to1.py
@@ -4,8 +4,16 @@
 #     i -= 1
 
 # Multiplying
-n = int(input("Enter the number:"))
-i = 1
-while i <= 10 :
-    print(n*i)
-    i += 1
+# n = int(input("Enter the number:"))
+# i = 1
+# while i <= 10 :
+#     print(n*i)
+#     i += 1
+
+# Print the element of folowing list 
+# [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
+num = [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
+idx = 0
+while idx < len(num):
+    print(num[idx])
+    idx += 1
