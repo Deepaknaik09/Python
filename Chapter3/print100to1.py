fe4274cd57ebe981b1e549adc92da97a34f5493e
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-02 07:26:17 +0530

    multiplication using while

--- Chapter3/print100to1.py
+++ Chapter3/print100to1.py
@@ -1,4 +1,11 @@
-i = 100
-while i >= 1 :
-    print(i)
-    i -= 1
+# i = 100
+# while i >= 1 :
+#     print(i)
+#     i -= 1
+
+# Multiplying
+n = int(input("Enter the number:"))
+i = 1
+while i <= 10 :
+    print(n*i)
+    i += 1
