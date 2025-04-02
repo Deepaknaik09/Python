cf6ce01fd4b380a140802586f6800e659efef712
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-02 23:04:14 +0530

    #

--- Chapter3/print100to1.py
+++ Chapter3/print100to1.py
@@ -12,8 +12,10 @@
 
 # Print the element of folowing list 
 # [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
-num = [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
-idx = 0
-while idx < len(num):
-    print(num[idx])
-    idx += 1
+
+# num = [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
+# idx = 0
+# while idx < len(num):
+#     print(num[idx])
+#     idx += 1
+print("hello")
