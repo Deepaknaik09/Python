6fce6bd09e67a52afbff04a7d65a804df52509b0
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-05 21:30:05 +0530

    range in for loop

---
+++ Chapter3/range.py
@@ -0,0 +1,25 @@
+# for i in range(10):
+#     print(i)
+
+# for i in range(2, 10):
+#     print(i)
+
+
+# for i in range(2, 10, 2):
+#     print(i)
+    
+# for i in range(100, 1, -1):
+#     print(i)
+
+# printing table
+# n=int(input("Enter a number:"))
+
+# for i in range(1, 11):
+#     print(n * i) 
+
+
+# pass statement
+# for i in range(5):
+#     pass #used for skip the work in for loop
+# print("useful work")
+
