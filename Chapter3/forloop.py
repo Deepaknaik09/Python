9d1292507dc53a19df01da06e838b9b4935ed127
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-05 21:08:35 +0530

    find x value using for loop

--- Chapter3/forloop.py
+++ Chapter3/forloop.py
@@ -6,7 +6,19 @@
 
 
 # String
-str = "Deepak"
+# str = "Deepak"
 
-for char in str:
-    print(char)
+# for char in str:
+#     print(char)
+
+
+# Finding number X in tuple
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
+x = 49
+
+idx = 0
+for ele in nums:
+    if(ele == x):
+        print("Number is found at index",idx)
+        break
+    idx += 1
