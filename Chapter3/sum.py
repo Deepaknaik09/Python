d68419cef39eec95122727bb56a0dcb42252b0ce
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-06 22:46:24 +0530

    pprint statement

--- Chapter3/sum.py
+++ Chapter3/sum.py
@@ -1,5 +1,23 @@
-n = 5
-sum = 0
+# n = 5
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+# print("total sum is:",sum)
+
+# n = 7
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+# print("factorial:",fact)
+
+# using for loop
+n = 7
+fact = 1
+
 for i in range(1, n+1):
-    sum += i
-print("total sum is:",sum)
+    fact *= i
+    
+print("factorial:",fact)
+print("hello")
