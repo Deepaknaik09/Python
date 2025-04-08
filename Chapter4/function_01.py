9f4c888659e87585fa196d1948ed7c414c798d72
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-08 22:36:30 +0530

    default arguments

--- Chapter4/function_01.py
+++ Chapter4/function_01.py
@@ -5,9 +5,15 @@
 # cal_sum(5, 10)
 
 # calculating the average of three number
-def cal_avg(a, b, c):
-    sum = a + b + c
-    avg = sum/3
-    print(avg)
-    return avg
-cal_avg(80, 23, 60)
+# def cal_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+# cal_avg(80, 23, 60)
+
+# default argument
+def cal_prod(a=1,b=2): #default argument 1 and 2
+    print(a * b)
+    return a * b
+cal_prod()
