e735ce474e29109e4b3742080976e52d79d06891
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-08 22:22:17 +0530

    calculating avg using func

--- Chapter4/function_01.py
+++ Chapter4/function_01.py
@@ -1,5 +1,13 @@
-def cal_sum(a, b):
-    sum = a + b
-    print(sum)
-    return sum
-cal_sum(5, 10)
+# def cal_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+# cal_sum(5, 10)
+
+# calculating the average of three number
+def cal_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+cal_avg(80, 23, 60)
