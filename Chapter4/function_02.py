94417d97c37ff54dda722197fe11daf33ce97730
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-09 21:35:20 +0530

    printing length of list

--- Chapter4/function_02.py
+++ Chapter4/function_02.py
@@ -1,8 +1,10 @@
 heros =["Ironman","Hulk","blackwido","blackpanter","spiderman"]
 cars =["bmw","benz","audi","suzuki","tata"]
+names =["deepak","kishor","vishal"]
 
 def print_len(list):
     print(len(list))
     
 print_len(heros)
 print_len(cars)
+print_len(names)
