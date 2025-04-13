f342e7ac4cce9d143bb03275cb0d1af6f9462446
Deepaknaik09 <Kr6379678@gmail.com>
2025-04-13 09:38:06 +0530

    Printing numbers using recursion

--- Chapter4/function_03.py
+++ Chapter4/function_03.py
@@ -1,7 +1,15 @@
-def cal_fact(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact *= i
-    print(fact)
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
     
-cal_fact(5)
+# cal_fact(5)
+
+#Recursion
+def show(n):
+    if (n == 0):
+        return
+    print(n)
+    show(n-1)
+show(5)
